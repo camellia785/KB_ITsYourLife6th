5168c34ed0cd57e0b07cbd5eaa446aa60c0d4662
camellia785 <choiyuna759@gmail.com>
2025-04-01 15:39:17 +0900

    Add components

---
+++ 03_UIUX/Irn02/03_vue/03_vue/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import CommonHeader from "./components/CommonHeader.vue";
+import MainGallery from "./components/MainGallery.vue";
+</script>
+
+<template>
+  <div>hi</div>
+</template>
+
+<style scoped></style>
---
+++ 03_UIUX/Irn02/03_vue/03_vue/src/components/MainGallery.vue
@@ -0,0 +1,13 @@
+<script setup>
+import BaseButton from "./UI/BaseButton.vue";
+</script>
+
+<template>
+  <!-- 그리드 컨테이너 -->
+  <main class="component-grid">
+    <!-- 컴포넌트 그룹(그리드아이템) -->
+    <section>h</section>
+  </main>
+</template>
+
+<style scoped></style>
