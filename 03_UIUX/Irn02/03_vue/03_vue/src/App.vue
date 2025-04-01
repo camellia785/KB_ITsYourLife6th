bc4ad382c1f6beb4b1b0216a2d231ae4dcbc68f7
camellia785 <choiyuna759@gmail.com>
2025-04-01 19:47:51 +0900

    Refactor App.vue

--- 03_UIUX/Irn02/03_vue/03_vue/src/App.vue
+++ 03_UIUX/Irn02/03_vue/03_vue/src/App.vue
@@ -1,6 +1,24 @@
-<script setup>
+<!-- <script setup>
 import CommonHeader from "./components/CommonHeader.vue";
 import MainGallery from "./components/MainGallery.vue";
+</script> -->
+
+<script>
+import { ref } from "vue";
+import CommonHeader from "./components/CommonHeader.vue";
+import MainGallery from "./components/MainGallery.vue";
+export default {
+  components: {
+    CommonHeader,
+    MainGallery,
+  },
+
+  setup() {
+    // composition api방식
+    const number = ref(0);
+    return { number };
+  },
+};
 </script>
 
 <template>
