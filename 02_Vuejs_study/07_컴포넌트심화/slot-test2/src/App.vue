109e163e89aee82686d5b39b2812046a033bb01e
camellia785 <choiyuna759@gmail.com>
2025-04-02 15:14:27 +0900

    Add Slot components

--- 02_Vuejs_study/07_컴포넌트심화/slot-test2/src/App.vue
+++ 02_Vuejs_study/07_컴포넌트심화/slot-test2/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import NoSlotTest from './components/NoSlotTest.vue'
+import SlotTest from './components/SlotTest.vue'
 </script>
 
 <template>
   <div>
     <NoSlotTest />
+    <br />
+    <SlotTest />
   </div>
 </template>
 
