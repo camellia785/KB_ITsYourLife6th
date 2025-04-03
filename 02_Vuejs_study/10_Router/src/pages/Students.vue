52bbef00825ee6eb56484fe6cdb1f2d046b9dd3a
camellia785 <choiyuna759@gmail.com>
2025-04-03 21:11:09 +0900

    Add Vue.js pages and components for routing

---
+++ 02_Vuejs_study/10_Router/src/pages/Students.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted } from "vue";
+import { useStudent } from "@/api/students";
+
+// 컴포저블함수 사용
+const { students, error, fetchAllstudents } = useStudent();
+onMounted(() => {
+  fetchAllstudents();
+});
+</script>
+
+<template>
+  <div>
+    <h2>KB-19기 학생들</h2>
+    <div>
+      <div v-for="student in students" :key="student.id">
+        <StudentCard :student="student"> </StudentCard>
+      </div>
+      <div v-if="error" class="error">
+        {{ error }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
