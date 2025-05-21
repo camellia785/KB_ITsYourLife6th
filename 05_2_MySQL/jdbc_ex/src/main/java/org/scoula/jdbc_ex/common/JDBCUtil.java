b929e518026905d980c278d0ea509ead13ca2a62
camellia785 <choiyuna759@gmail.com>
2025-05-21 14:11:11 +0900

    SQL jdbc lecture

---
+++ 05_2_MySQL/jdbc_ex/src/main/java/org/scoula/jdbc_ex/common/JDBCUtil.java
@@ -0,0 +1,20 @@
+package org.scoula.jdbc_ex.common;
+
+import java.sql.Connection;
+
+public class JDBCUtil {
+
+    // 1. 드라이버 설정, 2. db연결이용해서 연결하는 것까지.
+    // --> Connection con; <-- con을 리턴!
+    static Connection conn = null;
+    public static Connection getConnection(){
+        if (conn != null) return conn;
+
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
