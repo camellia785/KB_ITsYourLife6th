76dcc8421a264c811230e4ce4215f6355803ddd4
camellia785 <choiyuna759@gmail.com>
2025-06-10 16:09:32 +0900

    spring mvc

--- 07_Spring2/spring-mvc/src/main/java/org/scoula/controller/ExampleController.java
+++ 07_Spring2/spring-mvc/src/main/java/org/scoula/controller/ExampleController.java
@@ -1,4 +1,55 @@
 package org.scoula.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@Log4j2
+@RequestMapping("/example")
 public class ExampleController {
+
+    @RequestMapping
+    public String hello(Model model){
+
+        log.info(" =======> hello");
+
+        /*
+        * Model
+        * - 컨트롤러가 View로 데이터를 전달할 때 사용하는 객체
+        * - key-value 쌍으로 데이터를 저장
+        * */
+
+        model.addAttribute("message", "신규핸들러메소드 호출");
+        return "page/mappingResult";
+    }
+
+    // /example/modify 로 들어오는 요청 처리
+    // method를 이용해 특정 http 메소드의 요청에만 처리하도록 제한.
+//    @RequestMapping(value = "/modify", method = RequestMethod.POST)
+    @PostMapping("/modify")
+    public String modify(Model model){
+        model.addAttribute("message", "수정용 핸들러 메소드 호출..");
+        return "page/mappingResult";
+    }
+
+    @GetMapping("/delete")
+    public String delete(Model model){
+        model.addAttribute("message", "삭제용핸들러메서드호출.. ");
+        return "page/mappingResult";
+    }
+
+    @GetMapping(value={"/multi/modify", "/multi/delete"})
+    public String modifyAndDelte(Model model) {
+        model.addAttribute("message", "수정 및 삭제 핸들러 매소드 호출..");
+        return "page/mappingResult";
+    }
+
+    /*
+    * @GetMapping
+    * @PostMapping
+    * @DeleteMapping
+    * @PutMapping
+    *  */
 }
