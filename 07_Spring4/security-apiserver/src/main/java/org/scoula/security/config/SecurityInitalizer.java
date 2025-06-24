18f33c743db5e6ef9f87c883fc22aab2d9bb3d41
camellia785 <choiyuna759@gmail.com>
2025-06-24 14:43:28 +0900

    Spring SecurityAPIserver

---
+++ 07_Spring4/security-apiserver/src/main/java/org/scoula/security/config/SecurityInitalizer.java
@@ -0,0 +1,6 @@
+package org.scoula.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitalizer extends AbstractSecurityWebApplicationInitializer {
+}
